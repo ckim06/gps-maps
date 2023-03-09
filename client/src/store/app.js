52664182150dcd87fd9c4e135e9949d67eef3b4d
Chris Kim <chris.kim@meragrp.com>
2023-03-09 15:02:25 -0800

    added markers

--- client/src/store/app.js
+++ client/src/store/app.js
@@ -3,21 +3,24 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 export const useAppStore = defineStore('app', {
   state: () => ({
-    devices:[]
+    devices: []
   }),
   getters: {
-    getDevices(state){
-        return state.devices
-      }
+    getDevices(state) {
+      return state.devices
+    },
+    getMarkers(state) {
+      return state.devices.map((device) => device.latest_accurate_device_point);
+    }
   },
   actions: {
     async fetchDevices() {
       try {
         const data = await axios.get('http://localhost:8080/devices')
         this.devices = data.data
-        }
-        catch (error) {
-          console.log(error)
+      }
+      catch (error) {
+        console.log(error)
       }
     }
   },
